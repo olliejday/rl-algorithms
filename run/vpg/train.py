23ab28f7a4752ae85ec7d92ddb1b830e75bf9edd
olliejday <olliejday@gmail.com>
2019-05-31 22:09:08 +0100

    Revert "vpg working :)"
    This reverts commit 912ca65d52b3528293a15af291265db714cf3c77.
    
    # Conflicts:
    #	run/vpg/train.py
    #	src/vpg/vpg.py
    #	todo.txt

--- run/vpg/train.py
+++ run/vpg/train.py
@@ -1,17 +1,16 @@
 import gym
 import os
-import argparse
 
 from src.vpg.vpg import VanillaPolicyGradients
 from src.vpg.utils import VPGTrainingLogger
-from src.vpg.models import fc_small, fc_medium, cnn_small, cnn_medium
+from src.vpg.models import fc_small
 from src.common.utils import set_global_seeds
 
 
-def train(env, exp_name, model_fn, debug=False, seed=123, n_iter=100, save_every=25, **kwargs):
+def train(env_name, exp_name, model_fn, debug=False, seed=1, n_iter=100, save_every=25, **kwargs):
     """
     General training setup.
-    :param env: Environment to train on
+    :param env_name: Environment name to train on
     :param exp_name: Experiment name to save logs to
     :param model_fn: Model function to call to generate the policy model (see src.vpg.models)
     :param debug: debug flag for seeding reproducibility vs performance
@@ -20,22 +19,22 @@ def train(env, exp_name, model_fn, debug=False, seed=123, n_iter=100, save_every
     :param save_every: number of iterations to save models at
     :param kwargs: arguments to pass to VPG model __init__
     """
+    env = gym.make(env_name)
+
     # Set random seeds
     set_global_seeds(seed, debug)
 
-    root_dir = os.path.dirname(os.path.realpath(__file__))
-    experiments_path = os.path.join(root_dir, "experiments", exp_name)
-
     vpg = VanillaPolicyGradients(model_fn,
                                  env,
-                                 experiments_path=experiments_path,
                                  **kwargs)
 
-    training_logger = VPGTrainingLogger(experiments_path, ["Model_fn: {}".format(model_fn.__name__),
-                                                           "Seed: {}".format(seed),
+    root_dir = os.path.dirname(os.path.realpath(__file__))
+    experiments_path = os.path.join(root_dir, "experiments", exp_name)
+    training_logger = VPGTrainingLogger(experiments_path, ["Env_name: {}".format(env_name),
+                                                                  "Model_fn: {}".format(model_fn.__name__),
+                                                                  "Seed: {}".format(seed),
+                                                           str(kwargs),
                                                            str(vpg)])
-
-    vpg.setup_graph()
 
     for itr in range(n_iter):
         buffer = vpg.sample_trajectories(itr)
@@ -58,40 +57,30 @@ def train(env, exp_name, model_fn, debug=False, seed=123, n_iter=100, save_every
     env.close()
 
 
-def train_cartpole(exp_name="vpg-cartpole"):
-    env = gym.make("CartPole-v0")
-    train(env, exp_name, fc_small, debug=True, nn_baseline=True, nn_baseline_fn=fc_small, min_timesteps_per_batch=5000,
-          learning_rate=5e-3, n_iter=50)
+def train_cartpole(exp_name="vpg-debug"):
+    env_name = "CartPole-v0"
+    model_fn = fc_small
+
+    train(env_name, exp_name, model_fn, debug=True, nn_baseline=True, nn_baseline_fn=fc_small,
+          min_timesteps_per_batch=5000)
 
 
-def train_inverted_pendulum(exp_name="vpg-inverted-pendulum"):
-    env = gym.make("InvertedPendulum-v2")
-    train(env, exp_name, fc_small, debug=True, nn_baseline=True, nn_baseline_fn=fc_small, min_timesteps_per_batch=5000,
-          discrete=False, learning_rate=0.01, n_iter=100, gamma=0.9)
+def train_inverted_pendulum(exp_name="vpg-debug"):
+    env_name = "InvertedPendulum-v2"
+    model_fn = fc_small
 
-
-def train_lander(exp_name="vpg-lander"):
-    env = gym.make("LunarLanderContinuous-v2")
-    train(env, exp_name, fc_small, debug=True, nn_baseline=True, nn_baseline_fn=fc_small,
-          discrete=False, min_timesteps_per_batch=40000, learning_rate=0.005, gradient_batch_size=40000)
-
-
-def train_pong(exp_name="vpg-pong"):
-    env = gym.make("Pong-v0")
-    train(env, exp_name, cnn_small, nn_baseline=True, nn_baseline_fn=cnn_small,
-          discrete=True, min_timesteps_per_batch=10000, learning_rate=0.005)
+    train(env_name, exp_name, model_fn, debug=True, nn_baseline=True, nn_baseline_fn=fc_small, discrete=False)
 
 
 if __name__ == "__main__":
-    options = {}
-    options['lander'] = train_lander
-    options['inverted-pendulum'] = train_inverted_pendulum
-    options['cartpole'] = train_cartpole
-    options['pong'] = train_pong
+    """
+    Example usage:
+    
+    env_name = "CartPole-v0"
+    exp_name = "vpg-debug"
+    model_fn = fc_small
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("experiment", choices=options.keys())
-
-    args = parser.parse_args()
-
-    options[args.experiment]()
+    train(env_name, exp_name, model_fn)
+    """
+    train_cartpole()
+    # train_inverted_pendulum()
